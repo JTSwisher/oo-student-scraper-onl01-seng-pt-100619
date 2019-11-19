7e7d38b1becb35b15cb94840458176b0076dc443
Jeff Swisher <jefftswisher@gmail.com>
2019-11-19 02:44:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,17 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
-    
-    
-    
-  end
+    index_page = NOKOGIRI::HTML(open(index_url))
 
+    students [
+      :name => index_page.css(.student-name).text
+      :location => index_page.css(.student-lcoation).text
+      :profile_url => index_page.css(a src ="href")
+    ]
+
+
+
+  end
   def self.scrape_profile_page(profile_url)
     
   end

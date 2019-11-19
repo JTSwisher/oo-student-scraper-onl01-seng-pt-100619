58c2b4c99bb82ee85dd034eec533fc9aa701893e
Jeff Swisher <jefftswisher@gmail.com>
2019-11-19 04:14:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -41,8 +41,6 @@ def self.scrape_profile_page(profile_url)
         else 
           student_hash[:blog] = link 
         end 
-        student_hash[:profile_quote] = profile_page.css(".profile_qoute").text if profile_page.css(".profile_qoute")
-        student_hash[:bio] = profile_page.css("div.description_holder p").text if profile_page.css("div.description_holder p")
       end 
     student_hash
   end
